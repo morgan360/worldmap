3100a07415cc38cece506b12297bb0cad0e9063d
Morgan McKnight <69460926+morgan360@users.noreply.github.com>
2024-03-18 15:58:21 +0000

    Accepts worlddata files
    Signed-off-by: Morgan McKnight <69460926+morgan360@users.noreply.github.com>

--- api/models.py
+++ api/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class WorldData(models.Model):
     worldKey = models.CharField(max_length=255, unique=True)
     dataFile = models.FileField(upload_to='worlddata/')
+
+    def __str__(self):
+        return self.worldKey
