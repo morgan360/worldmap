e5319b2d8c2424037d9aded2bcaafad21b803c28
tmoosting <40519405+tmoosting@users.noreply.github.com>
2024-03-31 15:37:47 +0200

    updated login page

--- home/forms.py
+++ home/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Feedback
+from allauth.account.forms import LoginForm as AllauthLoginForm
 
 
 class FeedbackForm(forms.ModelForm):
@@ -15,3 +16,22 @@ from django import forms
 class ContactForm(forms.Form):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-input', 'placeholder': 'Your email'}))
     message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-textarea', 'placeholder': 'Your message'}))
+
+
+
+
+
+class CustomLoginForm(AllauthLoginForm):
+    def __init__(self, *args, **kwargs):
+        super(CustomLoginForm, self).__init__(*args, **kwargs)
+        for fieldname, field in self.fields.items():
+            if fieldname == 'remember':
+                # Only apply specific classes to the checkbox, remove 'w-full'
+                field.widget.attrs.update({
+                    'class': 'form-checkbox h-5 w-5 text-blue-600'
+                })
+            else:
+                # For other fields, keep 'w-full' and other classes
+                field.widget.attrs.update({
+                    'class': 'w-full bg-gray-600 text-white rounded border border-gray-600 p-3 leading-tight'
+                })
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+
+from home.views import CustomLoginView
 from .views import my_signup_view, update_profile, view_profile
 
 urlpatterns = [
     path('accounts/signup/', my_signup_view, name='signup'),
+    path('accounts/login/', CustomLoginView.as_view(), name='account_login'),
     path('profile/update/', update_profile, name='update_profile'),
     path('profile/', view_profile, name='view_profile'),
 ]
