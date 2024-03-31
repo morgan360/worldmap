0c78ba1f52d71ad49ca36ff7f06a9b5d0b7f4d5d
tmoosting <40519405+tmoosting@users.noreply.github.com>
2024-03-31 13:59:37 +0200

    tailwind install, about page restyle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    // Paths to your templates
+    './templates/**/*.html',
+    './static/js/**/*.js',
+    // any other paths where you may be using Tailwind classes
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
