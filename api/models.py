cf6614771505a4e7e52078d9c28a257405ac397b
OnlyWorlds <onlyworldsdev@gmail.com>
2024-07-18 14:18:14 +0200

    webgl reload, use venv db pass

--- api/models.py
+++ api/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
+from django.conf import settings
 
 class WorldData(models.Model):
     worldKey = models.CharField(max_length=255, unique=True)
     dataFile = models.FileField(upload_to='worlddata/')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='worlds')
 
     def __str__(self):
         return self.worldKey
